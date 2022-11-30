35e6dd7fbee36129f12491742dc9796cdc5f430d
maratangsoft <maratangsoft@gmail.com>
2022-11-30 23:58:02 +0900

    intro를 스플래시로 변경

--- app/src/main/java/com/maratangsoft/mykaraokebook/SplashActivity.kt
+++ app/src/main/java/com/maratangsoft/mykaraokebook/SplashActivity.kt
@@ -1,20 +1,14 @@
 package com.maratangsoft.mykaraokebook
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.widget.Toast
-import androidx.activity.result.ActivityResultCallback
-import androidx.activity.result.contract.ActivityResultContracts
 
-class IntroActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
 
         loadPref()
         goToMain()
